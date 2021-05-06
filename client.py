3eed16f109fce83bed2d910d679b5996db3cafc2
Julio Damasceno <xjulio@gmail.com>
2021-05-06 01:33:47 +0100

    Incluindo metodo para ler csv

--- client.py
+++ client.py
@@ -1,4 +1,20 @@
 import requests
 
-print("hello")
-print("hello de novo")
+def ler_csv():
+    macs = {}
+    with open('macs.csv', "r") as fp:
+        lines = fp.readlines()
+        #macs = [line.rstrip() for line in lines]
+        # mesmo coisa
+        if lines:
+            for line in lines:
+                line = line.rstrip()
+                line_array = line.split(";")
+                ip = line_array[0]
+                mac = line_array[1]
+                macs[ip] = mac
+
+    return macs
+
+m = ler_csv()
+print(m)
